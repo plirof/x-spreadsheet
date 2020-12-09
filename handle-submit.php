a9d6190fc3df159962370fcab4a5f10913015194
aaa <aaa@aaa.com>
2020-12-09 08:46:37 +0200

    201208c -trying submit test07submit.php

---
+++ handle-submit.php
@@ -0,0 +1,36 @@
+<?php
+/*
+200902 - initial version - different file per submission 
+Changes
+
+*/
+// file name different each time:  sn101-B2-DATE-paradopoulos.htm
+// file contents : textarea + sn101-B2-DATE-paradopoulos
+
+//$schoolname-
+
+print_r ($_REQUEST);
+
+
+$mytext=$_REQUEST["text_entered"] ;
+
+echo "<HR>";
+//$cur_date= date('Ymd');
+$file_name="sch".$_REQUEST["schoolname"]."-".$_REQUEST["taksi"].$_REQUEST["tmima"]."-".date('Ymd')."-".$_REQUEST["name"].".htm";
+
+echo $file_name;
+//$file_name=$cur_date;
+
+
+$myfile = fopen("word_saved_data/".$file_name, "w") or die("Unable to open file!");
+$txt = "<h2>$file_name</h2>\n";
+fwrite($myfile, $txt);
+$txt = "$mytext\n";
+fwrite($myfile, $txt);
+fclose($myfile);
+
+
+
+
+
+?>
